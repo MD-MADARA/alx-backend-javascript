ec3bbb9d6e681721b8076d66203e1b1ebf677323
MD-MADARA <mouadotaku9421@gmail.com>
2024-04-20 17:46:10 +0100

    fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   let arr = [];
   for (const employeesList of Object.values(report.allEmployees)) {
-    arr = [...employeesList, ...arr];
+    arr = [...arr, ...employeesList];
   }
 
   return arr;
