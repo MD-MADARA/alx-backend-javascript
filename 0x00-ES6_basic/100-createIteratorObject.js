c8f542e328baeb52b262d67e86345a0be859063a
MD-MADARA <mouadotaku9421@gmail.com>
2024-04-20 17:28:39 +0100

    13. Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  let arr = [];
+  for (const employeesList of Object.values(report.allEmployees)) {
+    arr = [...employeesList, ...arr];
+  }
+  
+  return arr;
+}
