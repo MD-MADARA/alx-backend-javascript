8002c1f2fb713ad61e9b5048e715d33e0ff1d712
MD-MADARA <mouadotaku9421@gmail.com>
2024-06-02 20:07:03 +0100

    fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
+
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
-  return [19, 20, 34].map((value) => new ClassRoom(value));
-}
+const initializeRooms = () => [19, 20, 34].map((n) => new ClassRoom(n));
+export default initializeRooms;
