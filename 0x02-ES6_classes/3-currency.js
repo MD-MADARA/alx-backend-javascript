09b5d955990a7e6d6ca564a87841ab4d5e81965d
MD-MADARA <mouadotaku9421@gmail.com>
2024-06-02 20:33:34 +0100

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+export default class Currency {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
