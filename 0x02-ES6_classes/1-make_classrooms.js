805ac79ef67ed5dc838f12e88d3d6e4d4ee95cb0
MD-MADARA <mouadotaku9421@gmail.com>
2024-06-02 21:48:20 +0100

    fix all

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import ClassRoom from './0-classroom';
 
 const initializeRooms = () => [19, 20, 34].map((n) => new ClassRoom(n));
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
