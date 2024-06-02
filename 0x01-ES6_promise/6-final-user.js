7a3cfc7e58dfca0de34d2fd5950fac5154d2fa33
MD-MADARA <mouadotaku9421@gmail.com>
2024-06-02 15:23:46 +0100

    lint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,11 @@
-#!/usr/bin/env node
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+export default function handleProfileSignup(firstName, lastName, photo) {
   return Promise
-    .allSettled([userPromise, photoPromise])
-    .then((results) => results.map(({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : reason.toString(),
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
+    .then((results) => results.map((obj) => ({
+      status: obj.status,
+      value: obj.status === 'fulfilled' ? obj.value : obj.reason.toString(),
     })));
 }
